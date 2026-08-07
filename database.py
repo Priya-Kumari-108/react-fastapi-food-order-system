9002c757aa7f17b3cb0b9ec6e6fecbe7870be5c3
Amit Bhateshwar <amitbhateshwar@gmail.com>
2026-08-07 20:10:09 +0530

    Save local backend code before linking to GitHub

---
+++ database.py
@@ -0,0 +1,12 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "food_order_db")
+
+client = AsyncIOMotorClient(MONGO_URL)
+database = client[DB_NAME]
+orders_collection = database.get_collection("orders")
